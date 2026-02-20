92e6438ff7b3c91fde32d9f6e8a4d5f511e2f469
aayushrambo8 <aayushrambo8@gmail.com>
2026-02-20 15:18:18 +0530

    Optimised Readings

--- Module2/JacobiMethod.java
+++ Module2/JacobiMethod.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class JacobiMethod
 {
 
-    static void readMatrix(Scanner obj, double[][] A, String title,int col)
-{
-    System.out.printf("Enter Elements of %s (M)",title);
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < col; j++)
-        {
-            System.out.printf("M%d%d: ", i + 1, j + 1);
-            A[i][j] = obj.nextDouble();
-        }
-}
+    static void readMatrix(Scanner obj, double[][] A, String title, int col)
+    {
+        System.out.printf("Enter Elements of %s (M)", title);
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < col; j++)
+            {
+                System.out.printf("M%d%d: ", i + 1, j + 1);
+                A[i][j] = obj.nextDouble();
+            }
+    }
 
     static void readInitialGuess(Scanner obj, double[][] X)
     {
@@ -59,14 +59,12 @@ public class JacobiMethod
 
                 double sum = B[i][0];
                 for (int j = 0; j < 3; j++)
-                {
                     if (j != i)
                         sum -= A[i][j] * xOld[j][0];
-                }
                 X[i][0] = sum / A[i][i];
             }
 
-            System.out.printf("%d\t\t %.6f\t %.6f\t %.6f\n",iteration, X[0][0], X[1][0], X[2][0]);
+            System.out.printf("%d\t\t %.6f\t %.6f\t %.6f\n", iteration, X[0][0], X[1][0], X[2][0]);
 
             double err = 0.0;
             for (int i = 0; i < 3; i++)
@@ -89,14 +87,14 @@ public class JacobiMethod
         double[][] B = new double[3][1];
         double[][] X = new double[3][1];
 
-        readMatrix(obj, A,"Matrix A",3);
-        readMatrix(obj, B,"Matrix B",1);
+        readMatrix(obj, A, "Matrix A", 3);
+        readMatrix(obj, B, "Matrix B", 1);
         readInitialGuess(obj, X);
 
         System.out.println("Enter no. of required Decimal Places and max Iterations");
         int epsilon = obj.nextInt();
         int maxIteration = obj.nextInt();
-        double tolerance = Math.pow(10,-epsilon);
+        double tolerance = Math.pow(10, -epsilon);
 
         jacobiMethod(A, B, X, maxIteration, tolerance);
 
